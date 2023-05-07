1d27188ea80ae1cdf3b8f929c344dd76b328ae39
Alexander <losev.pavlovo@mail.ru>
2023-05-07 10:28:46 +0300

    go

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ function toggleMenu() {
   menuToggle.classList.toggle("active");
   menu.classList.toggle("active");
 }
+
+// scroll
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "85px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".home__text,.about__img,.heading,.services__content,.work__text,.app h3,form input", {}),
+  sr.reveal(".home__img,.social,.about__text,.box,.work__img,.app,form textarea", { delay: 200 });
